0bc8565c9acfed95cca0a611cf4c465d2245217a
Keshawn Thomas <XZX6HW@NAM.CORP.GM.COM>
2023-04-02 13:45:01 -0400

    Project is added to gitHub

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { WorkComponent } from './work/work.component';
+import { SkillsComponent } from './skills/skills.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactFormComponent,
+    WorkComponent,
+    SkillsComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
