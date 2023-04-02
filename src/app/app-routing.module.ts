0e698facfd8310c9c4dcabdb2375b98bcc115ea1
Keshawn Thomas <XZX6HW@NAM.CORP.GM.COM>
2023-04-02 14:20:35 -0400

    Navigation bar and routing set

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { WorkComponent } from './work/work.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'skills',
+    component: SkillsComponent,
+  },
+  {
+    path: 'work',
+    component: WorkComponent,
+  },
+  {
+    path: 'contact-form',
+    component: ContactFormComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
